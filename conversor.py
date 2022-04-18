a16148af525b7384f609a2c5548c7958f43042e7
ROYCE MAYCOL <74213068@certus.edu.pe>
2022-04-17 21:49:33 -0500

    tareas python
    ejercicios

--- conversor.py
+++
@@ -1,7 +0,0 @@
-soles = input("¿Cuantos soles peruanos tienes?: ")
-soles = float(soles)
-valor_dolar_sunat_17_abril_2022 = 3.77
-dolares = soles / valor_dolar_sunat_17_abril_2022
-dolares = round(dolares, 3)
-dolares = str(dolares)
-print("tiens $" + dolares)
